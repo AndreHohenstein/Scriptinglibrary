30cf8c86b3956b6987a348df1469c3eb5d6586a6
Andre Hohenstein <68845584+AndreHohenstein@users.noreply.github.com>
2023-07-24 16:37:06 +0200

    Update install-Microsoft.UI.Xaml.ps1

--- install-Microsoft.UI.Xaml.ps1
+++ install-Microsoft.UI.Xaml.ps1
@@ -1,61 +1,21 @@
 try {
 # Install Windows UI Library
-$version = [Environment]::OSVersion.Version.ToString(2)
-$build   = (Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion").ReleaseId
 $OSName  = ((Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion" )  |
-               Where-Object {$_.ProductName -like "Windows 10*" -or $_.ProductName -like "Windows Server 2022*"}).ProductName
+               Where-Object {$_.ProductName -like "Windows Server 2022*"}).ProductName
 
-    if ($version -ge "10.0") {
-      if ($build -ge "1809") {
-        if ($OSName -like "Windows 10*" -or $OSName -like "Windows Server 2022*") {
+        if ($OSName -like "Windows Server 2022*") {
            # Current Windows Terminal version with PS Core
            Import-Module Appx -UseWindowsPowerShell -WarningAction SilentlyContinue
-           $msUILiboff = (Get-AppxPackage Microsoft.UI.Xaml.2.8).version
-           }
-      }
-   }
+    }
+
 } catch {
       Write-Host $_.Exception.Messege
      exit 1
 }
 
-# getting latest Windows UI Library
-$url             = 'https://github.com/microsoft/microsoft-ui-xaml/releases/latest'
-$request         = [System.Net.WebRequest]::Create($url)
-$response        = $request.GetResponse()
-$realTagUrl      = $response.ResponseUri.OriginalString
-$version         = $realTagUrl.split('/')[-1].Trim('v')
-$msUILibon        = $version.Substring(0,3)
-$fileName        = "Microsoft.UI.Xaml.$msUILibon.x64.appx"
-$realDownloadUrl = $realTagUrl.Replace('tag', 'download') + '/' + $fileName
-
-# check and install Windows UI Library package
-
-if ([string]"$msUILiboff" -ge [string]"$version") {
-
-    Write-Host "Your Installed Windows UI Library packages :"$($msUILiboff)"is equal or greater than $($version)" -ForegroundColor Green
-
-} else {
-
-    Write-Host "Download Windows UI Library packages $($version)" -ForegroundColor Green
-
-    $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile($realDownloadUrl, $env:USERPROFILE+ "\Downloads\$fileName")
-
-    Start-Sleep -Seconds 5
-
-    # CheckSum
-    $msUILibonhash  = (Get-FileHash -InputStream ($webClient.OpenRead($realDownloadUrl))).Hash
-    $msUILiboffhash = (Get-FileHash -Path $env:USERPROFILE\Downloads\$fileName).Hash
-
-    # Compute the hash value of a stream and verify the local file
-    if ($msUILibonhash  -eq $msUILiboffhash) {
-
-       Write-Host "CheckSum OK" -ForegroundColor Green
-
-       Write-Host "Install Windows UI Library packages $($($version))" -ForegroundColor Green
-
-       Add-AppxPackage -Path $env:USERPROFILE\Downloads\$fileName
-       Start-Sleep -Seconds 1
-   }
-}
+# Download Windows UI Library 2.7.3
+$ProgressPreference = 'SilentlyContinue'
+$url                = 'https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.3'
+Invoke-WebRequest -Uri $url -OutFile $env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.7.3.zip
+Expand-Archive -Path $env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.7.3.zip -DestinationPath .\Microsoft.UI.Xaml.2.7.3
+Add-AppPackage -Path "$env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.7.3\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx"
