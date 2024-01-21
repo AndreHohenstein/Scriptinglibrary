f9581580f62d12a3bdd8bff0f841fdada0974a79
Andre Hohenstein <68845584+AndreHohenstein@users.noreply.github.com>
2024-01-21 11:46:02 +0100

    Update install-Microsoft.UI.Xaml.ps1

--- install-Microsoft.UI.Xaml.ps1
+++ install-Microsoft.UI.Xaml.ps1
@@ -1,21 +1,19 @@
-try {
-# Install Windows UI Library
-$OSName  = ((Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion" )  |
-               Where-Object {$_.ProductName -like "Windows Server 2022*"}).ProductName
-
-        if ($OSName -like "Windows Server 2022*") {
-           # Current Windows Terminal version with PS Core
-           Import-Module Appx -UseWindowsPowerShell -WarningAction SilentlyContinue
-    }
-
-} catch {
-      Write-Host $_.Exception.Messege
-     exit 1
-}
+Import-Module Appx -UseWindowsPowerShell -WarningAction SilentlyContinue
 
 # Download Windows UI Library 
-$ProgressPreference = 'SilentlyContinue'
-$url                = 'https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.8'
-Invoke-WebRequest -Uri $url -OutFile $env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.8.zip
-Expand-Archive -Path $env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.8.zip -DestinationPath $env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.8
-Add-AppPackage -Path "$env:USERPROFILE\Downloads\Microsoft.UI.Xaml.2.8\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.8.appx"
+$wturl        = 'https://github.com/microsoft/terminal/releases/latest'
+$wtrequest    = [System.Net.WebRequest]::Create($wturl)
+$wtresponse   = $wtrequest.GetResponse()
+$realTagUrl   = $wtresponse.ResponseUri.OriginalString
+$wton         = $realTagUrl.split('/')[-1].Trim('v')
+$wtfileName   = "Microsoft.WindowsTerminal_"+"$wton"+"_8wekyb3d8bbwe.msixbundle_Windows10_PreinstallKit.zip"
+$realwtUrl    = $realTagUrl.Replace('tag', 'download') + '/' + $wtfileName
+
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($realwtUrl, $env:USERPROFILE+ "\Downloads\$wtfileName")
+
+Expand-Archive -Path $env:USERPROFILE\Downloads\$wtfileName -DestinationPath $env:USERPROFILE\Downloads\Microsoft.UI
+
+$File = (Get-ChildItem -Path C:\Users\Andre\Downloads\UI | Where-Object {$_.Name -match "x64"}).Name
+
+Add-AppPackage -Path "$env:USERPROFILE\Downloads\Microsoft.UI\$file"
