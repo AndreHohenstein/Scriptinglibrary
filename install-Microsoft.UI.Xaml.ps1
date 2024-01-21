b8df6ce69e07ab9a9e5bc185880e7b515671962c
Andre Hohenstein <68845584+AndreHohenstein@users.noreply.github.com>
2024-01-21 12:18:45 +0100

    Update install-Microsoft.UI.Xaml.ps1

--- install-Microsoft.UI.Xaml.ps1
+++ install-Microsoft.UI.Xaml.ps1
@@ -6,14 +6,21 @@ $wtrequest    = [System.Net.WebRequest]::Create($wturl)
 $wtresponse   = $wtrequest.GetResponse()
 $realTagUrl   = $wtresponse.ResponseUri.OriginalString
 $wton         = $realTagUrl.split('/')[-1].Trim('v')
-$wtfileName   = "Microsoft.WindowsTerminal_"+"$wton"+"_8wekyb3d8bbwe.msixbundle_Windows10_PreinstallKit.zip"
-$realwtUrl    = $realTagUrl.Replace('tag', 'download') + '/' + $wtfileName
+$msUI   = "Microsoft.WindowsTerminal_"+"$wton"+"_8wekyb3d8bbwe.msixbundle_Windows10_PreinstallKit.zip"
+$realwtUrl    = $realTagUrl.Replace('tag', 'download') + '/' + $msUI
 
 $webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($realwtUrl, $env:USERPROFILE+ "\Downloads\$wtfileName")
+$webClient.DownloadFile($realwtUrl, $env:USERPROFILE+ "\Downloads\$msUI")
 
-Expand-Archive -Path $env:USERPROFILE\Downloads\$wtfileName -DestinationPath $env:USERPROFILE\Downloads\Microsoft.UI
+$Path = $env:USERPROFILE +"\Downloads\" +"\Microsoft.UI"
+if(!(Test-Path $Path)) { New-Item -ItemType Directory -Path $Path | Out-Null} 
 
-$File = (Get-ChildItem -Path C:\Users\Andre\Downloads\UI | Where-Object {$_.Name -match "x64"}).Name
+Expand-Archive -Path $env:USERPROFILE\Downloads\$msUI -DestinationPath $Path -Force
 
-Add-AppPackage -Path "$env:USERPROFILE\Downloads\Microsoft.UI\$file"
+$File = (Get-ChildItem -Path $Path | Where-Object {$_.Name -match "x64"}).Name
+
+Add-AppPackage -Path $Path\$File
+Start-Sleep -Seconds 3
+
+# ! delete downloaded files:
+Remove-Item -Path $Path,$env:USERPROFILE\Downloads\$msUI -Recurse -Confirm:$false
